71baa251ec850ff1e84d6551bfb2ac62806a6a22
Jonathan Fallis <jonathanfallis@poweronplatforms.com>
2021-01-22 18:52:04 +0000

    22/01/21

--- Website Scripts/Add-PinnedItemsToTaskbar.ps1
+++ Website Scripts/Add-PinnedItemsToTaskbar.ps1
@@ -17,22 +17,20 @@
 [CmdletBinding()]
 param(
     [Parameter(Mandatory)]
-    [ValidateSet('Internet Explorer','MDT', 'ADUAC', 'DHCP', 'DNS', 'GPO', 'WSUS', 'WDS')]
+    [ValidateSet('IE','MDT', 'ADUAC', 'DHCP', 'DNS', 'GPO', 'WSUS', 'WDS')]
     [string]$Option
 )
 
 #Constants
 $shell = new-object -com 'Shell.Application'  
 
-
 Switch ($Option) {
-    "0" {$folder = $shell.Namespace("C:\Program Files\Internet Explorer"); $item = $folder.Parsename("iexplore.exe"); $item.invokeverb("taskbarpin")}
-    "1" {$folder = $shell.Namespace("C:\Program Files\Microsoft Deployment Toolkit\Bin"); $item = $folder.Parsename("DeploymentWorkbench.msc"); $item.invokeverb("taskbarpin")}
-    "2" {$folder = $shell.Namespace("c:\windows\system32"); $item = $folder.Parsename("dsa.msc"); $item.invokeverb("taskbarpin")}
-    "3" {$folder = $shell.Namespace("c:\windows\system32"); $item = $folder.Parsename("dhcpmgmt.msc"); $item.invokeverb("taskbarpin")}
-    "4" {$folder = $shell.Namespace("c:\windows\system32"); $item = $folder.Parsename("dnsmgmt.msc"); $item.invokeverb("taskbarpin")}
-    "5" {$folder = $shell.Namespace("c:\windows\system32"); $item = $folder.Parsename("gpmc.msc"); $item.invokeverb("taskbarpin")}
-    "6" {$folder = $shell.Namespace("c:\program files\update services\administrationSnapin"); $item = $folder.Parsename("wsus.msc"); $item.invokeverb("taskbarpin")}
-    "7" {$folder = $shell.Namespace("c:\windows\system32"); $item = $folder.Parsename("WdsMgmt.msc"); $item.invokeverb("taskbarpin")}
-
+    "IE" {$folder = $shell.Namespace("C:\Program Files\Internet Explorer"); $item = $folder.Parsename("iexplore.exe"); $item.invokeverb("taskbarpin")}
+    "MDT" {$folder = $shell.Namespace("C:\Program Files\Microsoft Deployment Toolkit\Bin"); $item = $folder.Parsename("DeploymentWorkbench.msc"); $item.invokeverb("taskbarpin")}
+    "ADUAC" {$folder = $shell.Namespace("c:\windows\system32"); $item = $folder.Parsename("dsa.msc"); $item.invokeverb("taskbarpin")}
+    "DHCP" {$folder = $shell.Namespace("c:\windows\system32"); $item = $folder.Parsename("dhcpmgmt.msc"); $item.invokeverb("taskbarpin")}
+    "DNS" {$folder = $shell.Namespace("c:\windows\system32"); $item = $folder.Parsename("dnsmgmt.msc"); $item.invokeverb("taskbarpin")}
+    "GPO" {$folder = $shell.Namespace("c:\windows\system32"); $item = $folder.Parsename("gpmc.msc"); $item.invokeverb("taskbarpin")}
+    "WSUS" {$folder = $shell.Namespace("c:\program files\update services\administrationSnapin"); $item = $folder.Parsename("wsus.msc"); $item.invokeverb("taskbarpin")}
+    "WDS" {$folder = $shell.Namespace("c:\windows\system32"); $item = $folder.Parsename("WdsMgmt.msc"); $item.invokeverb("taskbarpin")}
 }
--- Website Scripts/Add-RightClickOptions.ps1
+++ Website Scripts/Add-RightClickOptions.ps1
@@ -1,17 +1,30 @@
 ﻿<#
-Script to create add more options to your right click menu
-
-Author:  Jonathan of www.deploymentshare.com
-Version: 1.0.0
-
+.SYNOPSIS
+   Add Options to the right click menu
+.DESCRIPTION
+   Can be used to add options to the right click menu on WIndows Systems
+.AUTHOR
+    Jonathan Fallis
+.VERSION
+    1.0.1 - Changed the script to have a parameter option and to make into a tool
+.EXAMPLE
+    .\Add-PinnedItemsToTaskbar -Option CopyTo
+    .\Add-PinnedItemsToTaskbar -Option MoveTo
+    .\Add-PinnedItemsToTaskbar -Option CMDHere
 #>
-Function Add-RightClickOptions    
-    {   
-        New-PSDrive -Name HKCR -PSProvider Registry -Root HKEY_CLASSES_ROOT   
-        New-Item "HKCR:\AllFilesystemObjects\shellex\ContextMenuHandlers\{C2FBB630-2971-11D1-A18C-00C04FD75D13}" -Force # Copy to Folder...   
-        New-Item "HKCR:\AllFilesystemObjects\shellex\ContextMenuHandlers\{C2FBB631-2971-11D1-A18C-00C04FD75D13}" -Force # Move to Folder...   
-        New-Item "HKCR:\Folder\shell\Open Command Prompt Here\command\" -Force   
-        Set-Item "HKCR:\Folder\shell\Open Command Prompt Here\command\" -Value "cmd.exe /k pushd %L" # Open CMD Prompt Here...   
-    } # Close Function   
-  
-Add-RightClickOptions
+
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory)]
+    [ValidateSet('CopyTo','MoveTo', 'CMDHere')]
+    [string]$Option
+)
+
+New-PSDrive -Name HKCR -PSProvider Registry -Root HKEY_CLASSES_ROOT
+
+Switch ($Option) {
+    "CopyTo" {New-Item "HKCR:\AllFilesystemObjects\shellex\ContextMenuHandlers\{C2FBB630-2971-11D1-A18C-00C04FD75D13}" -Force}
+    "MoveTo" {New-Item "HKCR:\AllFilesystemObjects\shellex\ContextMenuHandlers\{C2FBB631-2971-11D1-A18C-00C04FD75D13}" -Force}
+    "CMDHere" {New-Item "HKCR:\Folder\shell\Open Command Prompt Here\command\" -Force; Set-Item "HKCR:\Folder\shell\Open Command Prompt Here\command\" -Value "cmd.exe /k pushd %L"}
+
+}
