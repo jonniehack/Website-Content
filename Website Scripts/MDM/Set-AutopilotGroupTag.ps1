63a240b92473c0504a3e27b9bc95d2751cc405a7
jonathanfallis@yahoo.co.uk <jonathanfallis@yahoo.co.uk>
2025-05-22 16:56:25 +0100

    Set-AutopilotGroupTag Updates

--- Website Scripts/MDM/Set-AutopilotGroupTag.ps1
+++ Website Scripts/MDM/Set-AutopilotGroupTag.ps1
@@ -1,147 +1,165 @@
 ﻿<#
 .SYNOPSIS
-    Installs Get-WindowsAuopilotInfo.ps1 and calls it using the parameter
+    Installs Get-WindowsAutopilotInfoCommunity.ps1 and calls it using the parameter
 .DESCRIPTION
-    * Used alongside a task sequence within Configuration Manager, this script was uses to add the device to autopilot and set a GroupTa
+    Used alongside a task sequence within Configuration Manager, this script was uses to add the device to autopilot and set a GroupTa
 .AUTHOR
     Jonathan Fallis - www.deploymentshare.com
+    Nick Benton - www.oddsandendpoints.co.uk
 .VERSION
+    1.2.3 -  Changed WinRM configuration
+    1.2.2 - Added Microsoft.Graph.Authentication to the required modules and requirement for WinRM
+    1.2.1 - Changed error handling to display in console and use transcript
+    1.2 - Improved logic
+    1.1 - Updated so the error handling works
+    1.0.2 - Updated to support Get-WindowsAutopilotInfoCommunity and empty groupTags
     1.0.1 - Added Error Logging
     1.0.0 - Original
 .EXAMPLE
-    .\Set-AutopilotGroupTag -TenantID "123456" -AppID "234567" -SecretID "345678" -GroupTag "AutopilotDevice"
+    .\Set-AutopilotGroupTag -tenantId "123456" -appId "234567" -appSecret "345678" -groupTag "AutopilotDevice"
 #>
 
 Param(
-    [Parameter(Mandatory=$true)]
-    [string]$TenantID,
-    [Parameter(Mandatory=$true)]
-    [string]$AppID,
-    [Parameter(Mandatory=$true)]
-    [string]$SecretID,
-    [Parameter(Mandatory=$true)]
-    [string]$GroupTag
+    [Parameter(Mandatory = $true)]
+    [string]$tenantId,
+    [Parameter(Mandatory = $true)]
+    [string]$appId,
+    [Parameter(Mandatory = $true)]
+    [string]$appSecret,
+    [Parameter(Mandatory = $false)]
+    [string]$groupTag
 )
 
+$ErrorActionPreference = 'stop'
+$workingDir = "$env:SystemDrive\Windows\Temp"
+$PowerShellDir = "$env:ProgramFiles\WindowsPowerShell\Modules"
+$logFile = "$workingDir\Set-AutopilotGroupTag.log"
 
-$WorkingDir = $env:TEMP
-$LogFilePath = "C:\Windows\Temp\Set-GroupTag.log"
-[System.Environment]::SetEnvironmentVariable('LOCALAPPDATA',"$env:SystemDrive\Windows\system32\config\systemprofile\AppData\Local")
-
-#Function for Error Logging
-Function Write-log {
-
-    [CmdletBinding()]
-    Param(
-        [parameter(Mandatory = $true)]
-        [String]$Path,
-
-        [parameter(Mandatory = $true)]
-        [String]$Message,
-
-        [parameter(Mandatory = $true)]
-        [String]$Component,
-
-        [Parameter(Mandatory = $true)]
-        [ValidateSet('Info', 'Warning', 'Error')]
-        [String]$Type
-    )
-
-    switch ($Type) {
-        'Info' { [int]$Type = 1 }
-        'Warning' { [int]$Type = 2 }
-        'Error' { [int]$Type = 3 }
-    }
-
-    # Create a log entry
-    $Content = "<![LOG[$Message]LOG]!>" + `
-        "<time=`"$(Get-Date -Format 'HH:mm:ss.ffffff')`" " + `
-        "date=`"$(Get-Date -Format 'M-d-yyyy')`" " + `
-        "component=`"$Component`" " + `
-        "context=`"$([System.Security.Principal.WindowsIdentity]::GetCurrent().Name)`" " + `
-        "type=`"$Type`" " + `
-        "thread=`"$([Threading.Thread]::CurrentThread.ManagedThreadId)`" " + `
-        "file=`"`">"
-
-    # Write the line to the log file
-    Add-Content -Path $Path -Value $Content
+try {
+    Stop-Transcript | Out-Null
 }
+catch [System.InvalidOperationException]
+{}
 
-#Test for internet connectivty using 8.8.8.8
-If (Test-Connection 8.8.8.8 -quiet) {
-    Write-Log -Type Info -Message "Internet Connection OK" -Component "Internet Check" -Path $LogFilePath
+Start-Transcript -Path $logFile -Append
+
+[System.Environment]::SetEnvironmentVariable('LOCALAPPDATA', "$env:SystemDrive\Windows\system32\config\systemprofile\AppData\Local")
+
+#Test for internet connectivity using 8.8.8.8
+If (Test-Connection 8.8.8.8 -Quiet) {
+    Write-Output 'Internet Connection OK'
 }
 Else {
-    Write-Log -Type Error -Message "Internet Connection check failed" -Component "Internet Check" -Path $LogFilePath ; Exit 1 
+    Write-Error 'Internet Connection check failed'
+    Stop-Transcript
+    Exit 1
 }
 
 #Enable TLS 1.2
 Try {
-        [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-        Write-Log -Type Info -Message "Setting TLS1.2 Completed Successfully" -Component "TLS 1.2 Check" -Path $LogFilePath
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    Write-Output 'Setting TLS1.2'
+
+}
+Catch {
+    Write-Error "Setting TLS1.2 - $($_.Exception | Out-String)"
+    Stop-Transcript
+    Exit 1
+}
+
+#Modules
+$PowerShellModules = @('PackageManagement', 'PowerShellGet', 'Microsoft.Graph.Authentication')
+foreach ($PowerShellModule in $PowerShellModules) {
+    $PowerShellModuleVer = Switch ($PowerShellModule) {
+        PackageManagement { '1.4.8.1' }
+        PowerShellGet { '2.2.5' }
+        Microsoft.Graph.Authentication { '2.9.1' }
+
     }
-Catch {
-        Write-Log -Type Error -Message ($_ | Out-String) -Component "TLS 1.2 Check" -Path $LogFilePath
-}
 
-#PowerShellGet from PSGallery URL
-Try {
-    if (!(Get-Module -Name PowerShellGet)){
-        $PowerShellGetURL = "https://psg-prod-eastus.azureedge.net/packages/powershellget.2.2.5.nupkg"
-        Write-Log -Type Info -Message "URL set to $PowerShellGetURL" -Component "PowerShellGet Check" -Path $LogFilePath
-        Invoke-WebRequest -UseBasicParsing -Uri $PowerShellGetURL -OutFile "$WorkingDir\powershellget.2.2.5.zip"
-        Write-Log -Type Info -Message "Downloaded PowerShellGet " -Component "PowerShellGet Check" -Path $LogFilePath
-        $Null = New-Item -Path "$WorkingDir\2.2.5" -ItemType Directory -Force
-        Expand-Archive -Path "$WorkingDir\powershellget.2.2.5.zip" -DestinationPath "$WorkingDir\2.2.5"
-        Write-Log -Type Info -Message "Unzipped PowerShellGet " -Component "PowerShellGet Check" -Path $LogFilePath
-        $Null = New-Item -Path "$env:ProgramFiles\WindowsPowerShell\Modules\PowerShellGet" -ItemType Directory -ErrorAction SilentlyContinue
-        Move-Item -Path "$WorkingDir\2.2.5" -Destination "$env:ProgramFiles\WindowsPowerShell\Modules\PowerShellGet\2.2.5"
-        Write-Log -Type Info -Message "Moved PowerShellGet to $WorkingDir" -Component "PowerShellGet Check" -Path $LogFilePath
+    $PowerShellModuleURL = Switch ($PowerShellModule) {
+        PackageManagement { "https://psg-prod-eastus.azureedge.net/packages/$($PowerShellModule.ToLower()).$PowerShellModuleVer.nupkg" }
+        PowerShellGet { "https://psg-prod-eastus.azureedge.net/packages/$($PowerShellModule.ToLower()).$PowerShellModuleVer.nupkg" }
+        Microsoft.Graph.Authentication { "https://psg-prod-eastus.azureedge.net/packages/$($PowerShellModule.ToLower()).$PowerShellModuleVer.nupkg" }
+    }
+
+    Try {
+        if (!(Get-Module -Name $PowerShellModule)) {
+
+            $PowerShellModuleZip = "$workingDir\$PowerShellModule.$PowerShellModuleVer.zip"
+            $PowerShellModulePath = "$workingDir\$PowerShellModule\$PowerShellModuleVer"
+            $PowerShellModulePF = "$PowerShellDir\$PowerShellModule\$PowerShellModuleVer"
+
+            Write-Output "URL set to $PowerShellModuleURL"
+            Invoke-WebRequest -UseBasicParsing -Uri $PowerShellModuleURL -OutFile $PowerShellModuleZip
+            Write-Output "Downloaded PowerShellModule $PowerShellModule"
+            $Null = New-Item -Path "$PowerShellModulePath" -ItemType Directory -Force
+            Expand-Archive -Path $PowerShellModuleZip -DestinationPath "$PowerShellModulePath" -Force
+            Write-Output "Unzipped $PowerShellModule"
+
+            if (!(Test-Path -Path "$PowerShellDir\$PowerShellModule" -PathType Container)) {
+                $Null = New-Item -Path "$PowerShellDir\$PowerShellModule" -ItemType Directory
+            }
+
+            if (Test-Path -Path $PowerShellModulePF -PathType Container) {
+                Remove-Item -Path $PowerShellModulePF -Force -Recurse
+                Write-Output "Removed existing $PowerShellModulePF"
+            }
+
+            Move-Item -Path "$PowerShellModulePath" -Destination $PowerShellModulePF -Force
+            Write-Output "Moved $PowerShellModule to $PowerShellModulePF"
         }
-}
-Catch {
-    Write-Log -Type Error -Message ($_ | Out-String) -Component "PowerShellGet Check" -Path $LogFilePath
-}
 
-#PackageManagement from PSGallery URL
-Try {
-    if (!(Get-Module -Name PackageManagement)){
-        $PackageManagementURL = "https://psg-prod-eastus.azureedge.net/packages/packagemanagement.1.4.7.nupkg"
-        Write-Log -Type Info -Message "URL set to $PackageManagementURL" -Component "PackageManagement Check" -Path $LogFilePath
-        Invoke-WebRequest -UseBasicParsing -Uri $PackageManagementURL -OutFile "$WorkingDir\packagemanagement.1.4.7.zip"
-        Write-Log -Type Info -Message "Downloaded PackageManagement" -Component "PackageManagement Check" -Path $LogFilePath
-        $Null = New-Item -Path "$WorkingDir\1.4.7" -ItemType Directory -Force
-        Expand-Archive -Path "$WorkingDir\packagemanagement.1.4.7.zip" -DestinationPath "$WorkingDir\1.4.7"
-        Write-Log -Type Info -Message "Unzipped PackageManagement" -Component "PackageManagement Check" -Path $LogFilePath
-        $Null = New-Item -Path "$env:ProgramFiles\WindowsPowerShell\Modules\PackageManagement" -ItemType Directory -ErrorAction SilentlyContinue
-        Move-Item -Path "$WorkingDir\1.4.7" -Destination "$env:ProgramFiles\WindowsPowerShell\Modules\PackageManagement\1.4.7"
-        Write-Log -Type Info -Message "Moved PackageManagement to $WorkingDir" -Component "PackageManagement Check" -Path $LogFilePath
+        Try {
+            Import-Module $PowerShellModule
+            Write-Output "$PowerShellModule Module Imported OK"
         }
-}
-Catch {
-    Write-Log -Type Error -Message ($_ | Out-String) -Component "PackageManagement Check" -Path $LogFilePath
+        Catch {
+            Write-Error "$PowerShellModule Import - $($_.Exception | Out-String)"
+            Stop-Transcript
+            Exit 1
+        }
+
+    }
+    Catch {
+        Write-Error "$PowerShellModule Module - $($_.Exception | Out-String)"
+        Stop-Transcript
+        Exit 1
+    }
 }
 
-#Import PowerShellGet
-if (Import-Module PowerShellGet) {
-    Write-Log -Type Info -Message "PowerShellGet Module Imported OK" -Component "PowerShellGet Import" -Path $LogFilePath
+#enable WinRM
+Try {
+    Set-WSManQuickConfig -Force -SkipNetworkProfileCheck
+    Write-Output 'Enabled WinRM'
 }
-Else {
-    Write-Log -Type Error -Message ($_ | Out-String) -Component "PowerShellGet Import" -Path $LogFilePath ; Exit 1 
+catch {
+    Write-Error "WinRM - $($_.Exception | Out-String)"
+    Stop-Transcript
+    Exit 1
 }
 
 #Install the script
-if (Install-Script Get-WindowsAutopilotinfo -Force) {
-    Write-Log -Type Info -Message "Get-WindowsAutopilotInfo Installed OK" -Component "Get-WindowsAutopilotInfo Install" -Path $LogFilePath
+Try {
+    Install-Script Get-WindowsAutopilotInfoCommunity -Force
+    Write-Output 'Get-WindowsAutopilotInfoCommunity Installed OK'
+    Try {
+        if (!$groupTag) {
+            $groupTag = ''
+        }
+        Get-WindowsAutopilotInfoCommunity -Online -tenantId $tenantId -appId $appId -appSecret $appSecret -groupTag $groupTag
+        Write-Output 'Get-WindowsAutopilotInfoCommunity executed successfully'
+        Stop-Transcript
+        exit 0
+    }
+    Catch {
+        Write-Error "Get-WindowsAutopilotInfoCommunity execution - $($_.Exception | Out-String)"
+        Stop-Transcript
+        Exit 1
+    }
 }
-Else {
-    Write-Log -Type Error -Message ($_ | Out-String) -Component "Get-WindowsAutopilotInfo Install" -Path $LogFilePath ; Exit 1 
-}
-
-#Run the script
-if (Get-WindowsAutopilotinfo -Online -TenantId $TenantID -AppId $AppID -AppSecret $SecretID -Grouptag $GroupTag) {
-    Write-Log -Type Info -Message "Get-WindowsAutopilotInfo executed successfully" -Component "Running Get-WindowsAutopilotInfo" -Path $LogFilePath
-}
-Else {
-    Write-Log -Type Error -Message ($_ | Out-String) -Component "Get-WindowsAutopilotInfo Install" -Path $LogFilePath ; Exit 1 
+Catch {
+    Write-Error "Get-WindowsAutopilotInfoCommunity Install - $($_.Exception | Out-String)"
+    Stop-Transcript
+    Exit 1
 }
